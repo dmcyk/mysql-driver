2e0e163a298229ab181e36271e9d70d4fe25de75
Damian Malarczyk <damian.malarczyk@gmail.com>
2016-09-15 21:41:51 +0200

    osjup/mysql using vdka/JSON

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 1),
+   		.Package(url: "https://github.com/osjup/mysql.git", majorVersion: 1),
    		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1),
     ]
 )
